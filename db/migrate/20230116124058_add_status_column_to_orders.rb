8cfb53116998573966fa15de626820c1cf397341
aleemchishti <chishti_aleem@yahoo.com>
2023-01-16 17:58:14 +0500

    order confirmation,code optimization and controller rspecs

---
+++ db/migrate/20230116124058_add_status_column_to_orders.rb
@@ -0,0 +1,5 @@
+class AddStatusColumnToOrders < ActiveRecord::Migration[7.0]
+  def change
+    add_column :orders, :status, :string, default: 'pending'
+  end
+end
--- spec/controllers/traveler_journeys_controller_spec.rb
+++ spec/controllers/traveler_journeys_controller_spec.rb
@@ -1,29 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Traveler::JourneysController, type: :controller do
-  # describe 'GET index' do
-  #   let!(:user) {create :user}
-  #   let!(:journey) {create :journey, user_id: user.id}
-  #   before(:each){
-  #    sign_in(user)
-  #   }
-  #   it 'assigns @journeys' do 
-  #    get :index
-  #    expect(assigns(:journeys)).to eq([journey])
-  #   end 
-  # end 
-
   describe "Create Journey" do
-    let(:user) { create :user } 
-    let(:journey) {create :journey,user_id:user.id}
-    before(:each){
-      sign_in(user)
-    }
-    it 'should redirect to journeys' do
-      post :create, params: {journey:{from:'ajdsj',to:'fasdj',departure:'Fri, 09 Dec 2022 02:08:49 -0500',rate:32,capacity:234}}
+    before(:each) do 
+     sign_in(user)
+    end 
+
+    let(:user) { create :user, role: User.roles[:traveler] }
+    
+    it 'should render new and create journey' do
+      get :new
+      expect(response).to render_template('traveler/journeys/new')
+      post :create, params: {journey: {from:'Lahore', to: 'Islamabad', departure: '2023-01-15 00:00:00 UTC', rate: 234, capacity: 234}}
+      # debugger
       expect(Journey.count).to eq(1)
-      expect(response).to redirect_to(traveler_journeys_path)
-      follow_redirect!
+      expect(response).to redirect_to(traveler_journeys_path) 
     end
-  end 
+  end
+
+  describe "Update Journey" do
+    let(:user) { create :user, role: User.roles[:traveler] }
+    
+    before(:each) do 
+     sign_in(user)
+    end 
+
+    
+    it 'should render edit and update journey' do
+     # debugger
+     get :edit
+     patch :update, params: {journey: {from:'gsd', to: 'Islamabad', departure: '2023-01-15 00:00:00 UTC', rate: 234, capacity: 234}}
+     expect(response).to redirect_to(traveler_journeys_path) 
+     expect(assigns[:journey].from).to eq('gsd')
+    end
+  end
 end
